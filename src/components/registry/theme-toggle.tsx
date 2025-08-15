72bac5025ea5de098ed76f753389732b9794ba87
SAF <sfamilleza@gmail.com>
2025-08-15 08:58:42 +0800

    Update theme-toggle.tsx

--- src/components/registry/theme-toggle.tsx
+++ src/components/registry/theme-toggle.tsx
@@ -40,7 +40,7 @@ export function ModeToggle() {
   };
 
   return (
-    <Button variant="outline" size="icon" onClick={toggleTheme}>
+    <Button variant="ghost" size="icon" onClick={toggleTheme}>
       <Sun className="dark:-rotate-90 h-4 w-4 rotate-0 scale-100 transition-all dark:scale-0" />
       <Moon className="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
