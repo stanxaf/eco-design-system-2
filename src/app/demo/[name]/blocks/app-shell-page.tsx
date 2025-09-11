15e7ae4697500ef0504e3ec426198517ed5c8b84
SAF <sfamilleza@gmail.com>
2025-09-11 22:12:16 +0800

    Update app-shell-page.tsx

--- src/app/demo/[name]/blocks/app-shell-page.tsx
+++ src/app/demo/[name]/blocks/app-shell-page.tsx
@@ -1,10 +1,7 @@
-import { TypographyH1 } from "@/components/ui/typography-h1";
-
 export default function AppShellPage() {
   return (
     <div className="flex min-h-[90vh] flex-col items-center justify-center gap-4 w-full">
       <div className="w-full text-muted-foreground text-center">
-        <TypographyH1>App Shell Template</TypographyH1>
         <p className="text-muted-foreground mt-2">
           This is your base layout with sidebar and main content area. Start adding components to customize it.
         </p>
