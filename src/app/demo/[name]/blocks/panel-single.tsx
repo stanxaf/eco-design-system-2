a795c40bd832f0b43e2999c9d45153472bd40742
SAF <sfamilleza@gmail.com>
2025-09-17 12:41:53 +0800

    fix: adding shell layout on app shell

--- src/app/demo/[name]/blocks/panel-single.tsx
+++ src/app/demo/[name]/blocks/panel-single.tsx
@@ -1,8 +1,13 @@
 import PanelSinglePage from "@/app/demo/[name]/blocks/panel-single-page";
+import ShellLayout from "@/app/demo/[name]/blocks/shell-layout";
 
 export const panelSingle = {
   name: "panel-single",
   components: {
-    Default: <PanelSinglePage />,
+    Default: (
+      <ShellLayout>
+        <PanelSinglePage />
+      </ShellLayout>
+    ),
   },
 };
