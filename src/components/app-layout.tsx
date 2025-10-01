3e55cfdb30464992a55c901a19048a61642593c2
SAF <sfamilleza@gmail.com>
2025-10-01 14:29:49 +0800

    fix

--- src/components/app-layout.tsx
+++ src/components/app-layout.tsx
@@ -19,11 +19,11 @@ export function AppLayout({
 
   return (
     <SidebarProvider defaultOpen={defaultSidebarOpen}>
-      <div className="flex min-h-screen">
+      <div className="flex min-h-screen bg-background text-foreground">
         <BrandSidebar />
         <div className="flex-1 flex flex-col">
-          {isMobile && <BrandHeader />}
-          <main className="flex-1 p-4">
+          <BrandHeader />
+          <main className="flex-1 p-4 bg-background">
             {children}
           </main>
         </div>
