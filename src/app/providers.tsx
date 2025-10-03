7a1169c66d5f1c1b00f8df9d516d207de3614d56
SAF <sfamilleza@gmail.com>
2025-10-03 11:55:22 +0800

    fix: missing search

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,17 +4,20 @@ import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { Toaster } from "@/components/ui/sonner";
 import { BrandHeader } from "@/components/brand-header";
 import { BrandSidebar } from "@/components/brand-sidebar";
+import { GlobalSearchProvider } from "@/components/global-search/context";
 import { type ReactNode } from "react";
 
 export default function Providers({ children }: { children: ReactNode }) {
   return (
-    <SidebarProvider defaultOpen={false}>
-      <BrandSidebar />
-      <SidebarInset>
-        <BrandHeader />
-        <main className="flex w-full flex-1 flex-col">{children}</main>
-      </SidebarInset>
-      <Toaster />
-    </SidebarProvider>
+    <GlobalSearchProvider>
+      <SidebarProvider defaultOpen={false}>
+        <BrandSidebar />
+        <SidebarInset>
+          <BrandHeader />
+          <main className="flex w-full flex-1 flex-col">{children}</main>
+        </SidebarInset>
+        <Toaster />
+      </SidebarProvider>
+    </GlobalSearchProvider>
   );
 }
